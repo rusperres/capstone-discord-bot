6cc68d47397b2feacd61adaa52d65c4493e3c0c8
Jazzey23 <145479778+Jazzey23@users.noreply.github.com>
2026-05-11 01:00:45 +0800

    matched the new tables fields

--- src/main/java/org/example/database/Classes/Ticket.java
+++ src/main/java/org/example/database/Classes/Ticket.java
@@ -1,24 +1,44 @@
 package org.example.database.Classes;
 
+
+import java.util.List;
+
 public class Ticket {
     private String ticketId;
-    private String discordThreadId;
     private String title;
     private String description;
     private String status;
-    private String prUrl;
-    private String claimedBy;
-    private String closedBy;
+    private String priority; // new update
+    private List<String> categories; // new update
 
-    public Ticket(String ticketId, String discordThreadId, String title, String description, String status, String prUrl, String claimedBy, String closedBy) {
+    private String discordThreadId;
+    private String prUrl;
+
+    private String claimedBy;
+
+    private String closedBy;
+    private String date_closed; // new update
+    private String date_added; // new update
+
+
+    public Ticket(String ticketId, String discordThreadId, String title, String description, String status, String prUrl, String claimedBy, String closedBy, String priority, List<String> categories, String date_added, String date_closed) {
         this.ticketId = ticketId;
-        this.discordThreadId = discordThreadId;
         this.title = title;
         this.description = description;
-        this.status = status;
+        this.priority = priority;
+        this.categories = categories;
+
+        this.discordThreadId = discordThreadId;
         this.prUrl = prUrl;
+
+
+        this.status = status;
+
         this.claimedBy = claimedBy;
+
         this.closedBy = closedBy;
+        this.date_closed = date_closed;
+        this.date_added = date_added;
 
         validateStatus();
     }
@@ -46,6 +66,10 @@ public class Ticket {
     public String getPrUrl() { return prUrl; }
     public String getClaimedBy() { return claimedBy; }
     public String getClosedBy() { return closedBy; }
+    public String getPriority() {return priority;}
+    public List<String> getCategories() {return categories;}
+    public String getDate_closed() {return date_closed;}
+    public String getDate_added() {return date_added;}
 
     // Setters
     public void setDiscordThreadId(String discordThreadId) { this.discordThreadId = discordThreadId; }
@@ -55,4 +79,6 @@ public class Ticket {
     public void setPrUrl(String prUrl) { this.prUrl = prUrl; }
     public void setClaimedBy(String claimedBy) { this.claimedBy = claimedBy; }
     public void setClosedBy(String closedBy) { this.closedBy = closedBy; }
+
+
 }
